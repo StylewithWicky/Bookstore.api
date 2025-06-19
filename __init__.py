f36c050b603e71dc59103d7153b4d685d15e0c2a
Yazz <itsjustmaitai@gmail.com>
2025-06-19 11:32:46 +0300

    Created the app.py and it upgraded the __Init.py

--- __init__.py
+++ __init__.py
@@ -1,18 +1,1 @@
-from flask import Flask
-from flask_sqlalchemy import SQLALCHEMY
-from flask_migrate import Migrate
-from config import Config
-
-db=SQLALCHEMY()
-migrate=Migrate()
-
-def create_app():
-    app=Flask(__name__)
-    app.config.from_object(Config)
-    db.init_app(app)
-    migrate.init_app(app,db)
-
-    from .controllers import register_routes
-    register_routes(app)
-    
-    return app
+from .app import create_app
