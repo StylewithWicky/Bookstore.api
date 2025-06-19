2411a9a36db044b15335da776ce665abbb3b04f7
Yazz <itsjustmaitai@gmail.com>
2025-06-19 13:33:15 +0300

    Created author.py

---
+++ models/author.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import relationship
+from . import db
+
+class Author(db.Model):
+    __tablename__='authors'
+
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String, nullable=False)
+
+    books=relationship ('Book',back_populates='author')
+
+    def to_dict(self):
+        {
+            'id': self.id,
+            'name': self.name,
+        }
